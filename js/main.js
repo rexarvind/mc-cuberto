10cb9814db457cdaf26801bf806738f15c10f827
Arvind Kumar <rexarvind.in@gmail.com>
2023-07-07 16:57:07 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (function () {
-    console.log('Cuberto Kiosk Design')
+    var myProdCards = document.querySelectorAll('.my-prod-card');
+    if (myProdCards) {
+        for (var i = 0; i < myProdCards.length; i++) {
+            myProdCards[i].addEventListener('click', function (e) {
+                e.preventDefault();
+                var el = this;
+                if (el.classList.contains('active')) {
+                    el.classList.remove('active');
+                } else {
+                    el.classList.add('active');
+                }
+            })
+        }
+    }
+
 })();
